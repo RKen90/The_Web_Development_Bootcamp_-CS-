d2ec21efc0ceb84ad31ae6a5838eb0cd0707e910
Robert Kennard <KennardR@cardiff.ac.uk>
2025-01-17 15:20:11 +0000

    JS Variables

---
+++ 14. JavaScript Basics/jsChallenge1.js
@@ -0,0 +1,8 @@
+//Our First Variables Exercise
+//In index.js, please define TWO variables:
+
+//Define a variable called myLuckyNumber and set it equal to 37.
+let myLuckyNumber = 37;
+
+//Define a variable called octopusLimbs and set it equal to 8.
+let octopusLimbs = 8;
