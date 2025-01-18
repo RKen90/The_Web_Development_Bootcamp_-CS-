f390fa52c41fb33b79c5d36937aca88bdf84d03a
Robert Kennard <KennardR@cardiff.ac.uk>
2025-01-18 13:34:19 +0000

    Constant Challenge

---
+++ 14. JavaScript Basics/constantExercise.js
@@ -0,0 +1,14 @@
+/*
+Our First Constants Exercise
+Let's get some practice defining constants. 
+In index.js, please define the following two variables:
+
+Define a constant called boilingPointC and set it to 100. 
+Define a second constant called boilingPointF and set it to 212. 
+*/
+
+const boilingPointC = 100;
+const boilingPointF = 212;
+
+console.log(boilingPointC);
+console.log(boilingPointF);
