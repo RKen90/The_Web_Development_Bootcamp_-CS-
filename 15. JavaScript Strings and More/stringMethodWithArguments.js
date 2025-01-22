261701f73897414e165f4bf6d6358972ece54623
Robert Kennard <KennardR@cardiff.ac.uk>
2025-01-22 13:35:21 +0000

    Methods with Arguments

---
+++ 15. JavaScript Strings and More/stringMethodWithArguments.js
@@ -0,0 +1,12 @@
+// String Methods with Arguments
+
+let message = "hello world";
+
+let tvShow = "catdog";
+
+tvShow.indexOf("cat");
+tvShow.indexOf("dog");
+tvShow.indexOf("z");
+
+let str = "supercalifragilisticexplailidocious";
+str.slice(0, 5);
