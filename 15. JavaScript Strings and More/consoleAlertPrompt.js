0cc90e5401676b89639e61c8598b7ae3f451a300
Robert Kennard <KennardR@cardiff.ac.uk>
2025-01-24 14:44:06 +0000

    console.log, alert. prompt

---
+++ 15. JavaScript Strings and More/consoleAlertPrompt.js
@@ -0,0 +1,33 @@
+/*
+Console.log()
+
+JavaScript Print
+
+console.log(“Hello World”);
+
+Alert
+
+alert(“Alerm, alerm, alerm!!”);
+
+Prompt
+
+prompt(“please enter a number”);
+
+
+*/
+
+console.log("Hello World");
+
+alert("alerm, alerm, alerm!!");
+
+prompt("please enter a number ");
+
+let number = prompt("please enter a number ");
+
+console.log(number);
+
+typeof number; // string
+
+parseInt(number + 9); // ParseInt finds the int in the string, type conversion
+
+`Your original number is ${number}, it's type is ${typeof number}. We use ParstInt, now your number is ${mumber2}, its type is ${typeof mumber2}`;
