e7105827c3420d477a65cf68a65552553604eb5d
Robert Kennard <KennardR@cardiff.ac.uk>
2025-01-24 14:24:24 +0000

    Comparison Operators

---
+++ 16. JavaScript Decision Making/decisionMakingWithCode.js
@@ -0,0 +1,11 @@
+/* Boolean Logic 
+
+Comparison Operators
+
+*/
+
+1 > 3; //Greater than
+1 < 3; //Less Than
+let age = 18;
+age >= 21; // Grather than or equal to
+age <= 21; //Less than or equal to
