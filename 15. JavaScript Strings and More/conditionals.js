ef871fcb9055e2d8a811d4de61532abe8e19ff1d
Robert Kennard <KennardR@cardiff.ac.uk>
2025-01-24 15:15:30 +0000

    If Statements

---
+++ 15. JavaScript Strings and More/conditionals.js
@@ -0,0 +1,11 @@
+alert("It's working!");
+
+if (1 + 1 === 2) {
+  console.log("So math still works");
+}
+
+let random = Math.random();
+if (random < 0.5) {
+  console.log("Your number is less than 0.5");
+  console.log(`Your number was ${random}`);
+}
